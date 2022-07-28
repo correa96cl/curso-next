a83e5d4d5f51bba28bbcead8b7443ec989a202b5
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2022-07-28 07:40:53 -0400

    First Commit

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Link from '../src/components/Link'
+
+export default function Page404() {
+    return (
+        <div>
+            <h1>Caiu na merda</h1>
+            <Link href="/">Merdinha</Link>
+
+        </div>
+    )
+}
---
+++ pages/faq.js
@@ -0,0 +1,11 @@
+import Link from '../src/components/Link'
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>FQA</h1>
+            <Link href="/">FAQ</Link>
+
+        </div>
+    )
+}
