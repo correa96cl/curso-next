7386364f94fa06e43784841ea0e9aee2befa40ec
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2022-08-05 22:37:49 -0400

    Commit final

--- pages/faq.js
+++ pages/faq.js
@@ -1,65 +1,24 @@
-import Link from '../src/components/Link'
-import Footer from '../src/components/patterns/Footer'
-import React, { } from 'react'
-import Head from 'next/head'
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen';
 
-export async function getServerSideProps() {
-    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+export default FAQScreen; 
 
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
-        .then((respostaServidor) => {
-            return respostaServidor.json();
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
         })
         .then((resposta) => {
             return resposta;
-        })
+        });
+
     return {
-        props: {
-            faq,
-        },
-    }
-}
-
-
-function Title({ children, as }) {
-
-
-    const Tag = as;
-    return (
-
-        <React.Fragment>
-
-            <Tag>{children}</Tag>
-            <style jsx>{`
-        ${Tag} {color: red;}
-        
-        `}
-
-            </style>
-        </React.Fragment>
-    )
-}
-
-export default function FAQPage({ faq }) {
-
-    return (
-        <div>
-            <Head>
-                <title>Home - Alura</title>
-            </Head>
-            <Title as="h2">FQA</Title>
-            <Link href="/">FAQ</Link>
-            <ul>
-                {faq.map(({ answer, question }) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                        </article>
-                        {answer}
-                    </li>
-                ))}
-            </ul>
-            <Footer />
-        </div>
-    )
-}
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
